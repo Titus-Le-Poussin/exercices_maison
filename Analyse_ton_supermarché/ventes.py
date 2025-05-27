c06c9b58914d498f9a549ae083268caef82c5bd6
Timothée <timotheeithier@gmail.com>
2025-05-27 23:56:26 +0200

    dernier avant dodo

--- Analyse_ton_supermarché/ventes.py
+++ Analyse_ton_supermarché/ventes.py
@@ -1,11 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
-file_path = r"C:\Users\Utilisateur\Documents\Informatique Etudes 2025\exercice\exo_maison\Analyse_ton_supermarché\ventes.csv"
+Dataframe = r"C:\Users\Utilisateur\Documents\Informatique Etudes 2025\exercice\exo_maison\Analyse_ton_supermarché\ventes.csv"
 
 try:
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(Dataframe)
     print(df.head())
+    df["Total"] = df["Quantite"] * df["Prix Unitaire"]
 except FileNotFoundError:
-    print(f"Le fichier {file_path} n'a pas été trouvé.")
+    print(f"Le fichier {Dataframe} n'a pas été trouvé.")
