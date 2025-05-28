907e112a48c7757baef5451b1aea7848638d6056
Timothée <timotheeithier@gmail.com>
2025-05-28 15:21:41 +0200

    ça avance, on affiche les produit et leur prix

--- Analyse_ton_supermarché/ventes.py
+++ Analyse_ton_supermarché/ventes.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from matplotlib.dates import DateFormatter
 
 
 Dataframe = r"C:\Users\Utilisateur\Documents\Informatique Etudes 2025\exercice\exo_maison\Analyse_ton_supermarché\ventes.csv"
@@ -12,5 +12,7 @@ try:
     df_trie = df.sort_values(by='Prix_unitaire', ascending=False)
     print("Voici les 5 produits les plus chers :")
     print(df_trie.head(5))
+    df_prix = df["Prix_unitaire"] * df["Quantite"]
+    print(df["Produit"], df_prix)
 except FileNotFoundError:
     print(f"Le fichier {Dataframe} n'a pas été trouvé.")
