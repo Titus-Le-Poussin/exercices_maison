e56ea6f000082576c771df7f223713949e7246a3
Timothée <timotheeithier@gmail.com>
2025-05-30 18:21:16 +0200

    Liste et trie correct

--- Analyse_ton_supermarché/ventes.py
+++ Analyse_ton_supermarché/ventes.py
@@ -8,15 +8,43 @@ Dataframe = r"C:\Users\Utilisateur\Documents\Informatique Etudes 2025\exercice\e
 try:
     df = pd.read_csv(Dataframe)
 
+    # Consigne: Afficher le prix total de tous les produits
+
     df["Total"] = df["Quantite"] * df["Prix_unitaire"]
     df_all_price = df["Total"].sum()
 
+
+
     df_trie_price = df.sort_values(by='Prix_unitaire', ascending=False)
+
+    # Consigne : Afficher le produit le plus vendu par quantité
     df_trie_quantity = df.sort_values(by='Quantite', ascending=False)
-    print(df)
+
+    # Consigne: Afficher le jour avec le plus de ventes
+    df_trie_date = df.sort_values(by='Total', ascending=False)
+
+
+
+    # Consigne: Afficher les premières lignes
+
+    #print(df.head(12))   
+    print(df_trie_date.head(12))
+
+    # Consigne : Afficher le chiffre d'affaire par catégorie
+
+    df_trie_category = df.sort_values(by='Categorie', ascending=True)
+    #df_trie_category.groupby("")
+
+    # les prints
+
     print("il y a", len(df), "produits en tout")
     print("Voici le produit le plus cher :", df_trie_price.iloc[0]["Produit"])
+    print("Voici le jour avec le plus de ventes : ", df_trie_date.iloc[0]["Date"], "avec le produit", df_trie_date.iloc[0]["Produit"])
     print("Voici le produit le plus vendu:", df_trie_quantity.iloc[0]["Produit"], "avec une quantite de", df_trie_quantity.iloc[0]["Quantite"])
+
+
     print("Le prix total de tous les produits est :", df_all_price)
+
+
 except FileNotFoundError:
     print(f"Le fichier {Dataframe} n'a pas été trouvé.")
