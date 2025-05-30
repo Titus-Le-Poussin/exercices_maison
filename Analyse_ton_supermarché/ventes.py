fbd9309bf1d4b6f3b83977f966f3aff80f4e5bc8
Timothée <timotheeithier@gmail.com>
2025-05-30 17:17:37 +0200

    c'est correctement trier

--- Analyse_ton_supermarché/ventes.py
+++ Analyse_ton_supermarché/ventes.py
@@ -7,12 +7,16 @@ Dataframe = r"C:\Users\Utilisateur\Documents\Informatique Etudes 2025\exercice\e
 
 try:
     df = pd.read_csv(Dataframe)
+
+    df["Total"] = df["Quantite"] * df["Prix_unitaire"]
+    df_all_price = df["Total"].sum()
+
+    df_trie_price = df.sort_values(by='Prix_unitaire', ascending=False)
+    df_trie_quantity = df.sort_values(by='Quantite', ascending=False)
     print(df)
     print("il y a", len(df), "produits en tout")
-    df_trie = df.sort_values(by='Prix_unitaire', ascending=False)
-    print("Voici les 5 produits les plus chers :")
-    print(df_trie.head(5))
-    df_prix = df["Prix_unitaire"] * df["Quantite"]
-    print(df["Produit"], df_prix)
+    print("Voici le produit le plus cher :", df_trie_price.iloc[0]["Produit"])
+    print("Voici le produit le plus vendu:", df_trie_quantity.iloc[0]["Produit"], "avec une quantite de", df_trie_quantity.iloc[0]["Quantite"])
+    print("Le prix total de tous les produits est :", df_all_price)
 except FileNotFoundError:
     print(f"Le fichier {Dataframe} n'a pas été trouvé.")
