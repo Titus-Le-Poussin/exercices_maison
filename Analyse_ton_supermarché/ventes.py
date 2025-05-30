c0d084c81445ca41055e25eb0cba58ae63a28b3f
Timothée <timotheeithier@gmail.com>
2025-05-30 19:06:50 +0200

    exo Data validé!

--- Analyse_ton_supermarché/ventes.py
+++ Analyse_ton_supermarché/ventes.py
@@ -14,12 +14,14 @@ try:
     df_all_price = df["CA"].sum()
 
     df_trie_price = df.sort_values(by='Prix_unitaire', ascending=False)
+    df_trie_price_by_date = df.groupby('Date')["CA"].sum().reset_index()
 
     # Consigne : Afficher le produit le plus vendu par quantité
     df_trie_quantity = df.sort_values(by='Quantite', ascending=False)
 
     # Consigne: Afficher le jour avec le plus de ventes
     df_trie_date = df.sort_values(by='CA', ascending=False)
+    df_sorted_by_date = df.sort_values(by="Date")
 
     # Consigne : Afficher le chiffre d'affaire par catégorie
     df_trie_category = df.sort_values(by='Categorie', ascending=True)
@@ -27,8 +29,8 @@ try:
     
 
     # les prints
-    print(df.head(12))
-    print("il y a", len(df), "produits en tout")
+    print(df.head(5))  # Consigne:Afficher Les premières lignes
+    print("il y a", len(df), "produits en tout") # Consigne:Afficher Le nombre total de lignes
     print("Voici le produit le plus cher :", df_trie_price.iloc[0]["Produit"])
     print("Voici le jour avec le plus de ventes : ", df_trie_date.iloc[0]["Date"], "avec le produit", df_trie_date.iloc[0]["Produit"])
     print("Voici le produit le plus vendu:", df_trie_quantity.iloc[0]["Produit"], "avec une quantite de", df_trie_quantity.iloc[0]["Quantite"])
@@ -40,7 +42,7 @@ try:
 
 
 
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(15, 10))
     plt.title("Ventes du supermarché")
     plt.xlabel("Date")
     plt.ylabel("Chiffre d'Affaires")
@@ -48,9 +50,8 @@ try:
     date_formatter = DateFormatter("%d/%m/%Y")
     plt.gca().xaxis.set_major_formatter(date_formatter)
     plt.xticks(rotation=45)
-    plt.bar(df_trie_date["Date"], df_trie_date["CA"], color='blue', label="Chiffre d'Affaires")
-    plt.yticks(range(0, int(df_trie_date.max()) + 1))
-    plt.bar(df_trie_date["Date"], df_trie_date["CA"], color='blue', label='Chiffre d\'Affaires')
+    plt.bar(df_trie_price_by_date["Date"], df_trie_price_by_date["CA"], color='blue', label="Chiffre d'Affaires")
+    plt.yticks(range(0, int(df_sorted_by_date["CA"].max()) + 1))
     plt.legend()
     plt.show()
     plt.tight_layout()
