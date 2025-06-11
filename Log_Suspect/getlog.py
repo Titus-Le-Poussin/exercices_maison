2ac8e3f9dae99692e84c0ebeeb386aeaa093ec2b
Timothée <timotheeithier@gmail.com>
2025-06-11 14:41:04 +0200

    toujours le trie

--- Log_Suspect/getlog.py
+++ Log_Suspect/getlog.py
@@ -7,7 +7,12 @@ try:
 
     df_sorted = df.sort_values(by='ip', ascending=True)
     print(df_sorted)
-
+    df_sorted_by_IP = df.groupby('ip').size()
+    print("Nombre d'IP differentes :", len(df_sorted_by_IP))
+    print("Voici le nombre de requetes par IP :")
+    print(df_sorted_by_IP)
+    print("Le/s log/s suspect est/sont:")
+    print(df_sorted_by_IP[df_sorted_by_IP > 3])
 
 except FileNotFoundError:
     print(f"Le fichier {LogData} n'a pas été trouvé.")
