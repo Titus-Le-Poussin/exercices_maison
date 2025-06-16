520f418bb0436b7d6df02b33fe3e5e766e048394
Timothée <timotheeithier@gmail.com>
2025-06-16 16:40:27 +0200

    avant la correction

--- Log_Suspect/getlog.py
+++ Log_Suspect/getlog.py
@@ -1,18 +1,30 @@
 import pandas as pd
+from datetime import datetime
 
 LogData = r"C:\Users\Utilisateur\Documents\Informatique Etudes 2025\exercice\exo_maison\Log_Suspect\log.txt"
 
 try:
     df = pd.read_csv(LogData, sep= ' - ', header = None, names = ['Date', 'ip'], engine = 'python')
+    df['Date'] = pd.to_datetime(df['Date'])
+
 
     df_sorted = df.sort_values(by='ip', ascending=True)
-    print(df_sorted)
     df_sorted_by_IP = df.groupby('ip').size()
     print("Nombre d'IP differentes :", len(df_sorted_by_IP))
     print("Voici le nombre de requetes par IP :")
     print(df_sorted_by_IP)
-    print("Le/s log/s suspect est/sont:")
-    print(df_sorted_by_IP[df_sorted_by_IP > 3])
+
+    suspects = []
+    for ip in df_sorted_by_IP.index:
+        dates = df[df['ip'] == ip]['Date'].sort_values().tolist()
+        for i in range(len(dates) - 4):
+            if (dates[i + 4] - dates[i]).total_seconds() < 300:
+                suspects.append(ip)
+                break
+    if suspects:
+        print("IP suspectes (5 requetes en moins de 5 minutes) :", suspects)
+    else:
+        print("Aucune IP suspecte trouvee.")
 
 except FileNotFoundError:
     print(f"Le fichier {LogData} n'a pas été trouvé.")
